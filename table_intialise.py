ff129d9a343c74336be08d800c4fed7da4c6a2c6
dj325 <dominic.jaques@gmail.com>
2016-07-15 14:13:04 +0100

    Update table_intialise.py

--- table_intialise.py
+++ table_intialise.py
@@ -1,48 +1,58 @@
-# Intialises the table with data from .pdb and .json files.
-# Retrieves contents of _scalr_statistics from .json and checks if file is 'SAD'
-# Initialises a table in SQLite with all this information.
-# The _scalr_statistics data is given as a list of length 3, only the first is
-# taken at the moment.
 
+# Locates _scalr_statistics
 import sqlite3
 import json
-import sys
-import os.path
 
-print sys.argv
+conn = sqlite3.connect('pdb_coordinates.sqlite')
+cur = conn.cursor()
 
+cur.executescript('''
+DROP TABLE IF EXISTS PDB_id;
+DROP TABLE IF EXISTS High_Res_Stats;
+DROP TABLE IF EXISTS Low_Res_Stats;
+DROP TABLE IF EXISTS Mid_Res_Stats;
+CREATE TABLE PDB_id (
+    id  TEXT UNIQUE,
+    data_type TEXT
+);
+CREATE TABLE High_Res_Stats (
+    pdb_id  TEXT UNIQUE
+);
+CREATE TABLE Low_Res_Stats (
+    pdb_id  TEXT UNIQUE
+);
+CREATE TABLE Mid_Res_Stats (
+    pdb_id  TEXT UNIQUE
+)
+''')
 
+names = {
+  'I/sigma' : 'IoverSigma',
+  'Completeness' : 'completeness',
+  'dI/s(dI)' : 'diffI',
+  'Rmerge(I+/-)' : 'RmergeIpim',
+  'Rmerge(I)' : 'RmergeI',
+  'Low resolution limit' : 'lowresolutionlimit',
+  'Rmeas(I)' : 'RmeasI',
+  'Anomalous slope' : 'anomalousslope',
+  'dF/F' : 'diffF',
+  'Wilson B factor' : 'wilsonbfactor',
+  'Rmeas(I+/-)' : 'RmeasIpim',
+  'High resolution limit' : ' highresolutionlimit',
+  'Rpim(I+/-)' : 'RpimIpim',
+  'Anomalous correlation' : 'anomalouscorrelation',
+  'Rpim(I)' : 'RpimI',
+  'Total observations' : 'totalobservations',
+  'Multiplicity' : 'multiplicity',
+  'Anomalous completeness' : 'anomalouscompleteness',
+  'CC half' : 'cchalf',
+  'Anomalous multiplicity' : 'anomalousmultiplicity',
+  'Total unique' : 'totalunique'
+}
+# Completeness only has a value for High_Res_Stats
 
-# Parses .json file
-fn_xia2 = 'xia2.json'
-fh_xia2 = json.load(open(fn_xia2))
-result = {}
-# Locates _scalr_statistics
-obj = fh_xia2["_crystals"]
-for name in obj.keys():
-    data_type = obj[name]['_wavelengths']
-    if 'SAD' in data_type.keys():
-        data_type = 'SAD'
-    result[name] = obj[name]['_scaler']['_scalr_statistics']['["AUTOMATIC", "%s", "SAD"]' % name]
-# Inserting pdb id and data type
-cur.execute( '''
-ALTER TABLE PDB_id ADD data_type TEXT''')
-cur.execute( '''N
-INSERT OR IGNORE INTO PDB_id
-(id, data_type) VALUES (?, ?) ''', (pdb_id, data_type))
-
-stat_list = []      # Will be used as column headings
-value_list = []     # Will be used as row values
-for item in result.values():
-    for stat in item:
-        value_list.append(item[stat])
-
-items = len(stat_list)
-for i in range(0, items):
-    cur.execute('''
-    UPDATE PDB_id SET {0} = ?
-    WHERE id = ? '''.format(stat_list[i]), (value_list[i][0], pdb_id))
-
-print "PDB id:", pdb_id
-print "Data Type:", data_type
-print 'Added', items, 'columns from "_scalr_statistics" to table.'
+for stat in names.values():
+    cur.executescript('''
+    ALTER TABLE High_Res_Stats ADD {0} TEXT;
+    ALTER TABLE Low_Res_Stats ADD {0} TEXT;
+    ALTER TABLE Mid_Res_Stats ADD {0} TEXT'''.format(stat))
