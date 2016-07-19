788eb3dcb0352fac91d4016853c9cbefbef15d82
dj325 <dominic.jaques@gmail.com>
2016-07-19 10:57:31 +0100

    Update pdb_parser.py

--- pdb_parser.py
+++ pdb_parser.py
@@ -1,10 +1,34 @@
 import sqlite3
+from os import path
+import argparse
+
+
+
+parser = argparse.ArgumentParser(description='command line argument')
+
+parser.add_argument('--pdb_id', dest = 'pdb_id', type=str,
+                    help='the pdb id', default = None)
+
+parser.add_argument('--directory', dest = 'directory', type=str,
+                    help='the pdb id directory', default = '/dls/mx-scratch/jcsg-data/PDB_coordinates/')
+
+
+args = parser.parse_args()
+# Checks the input is sane.
+if args.pdb_id is None:
+    print 'User must supply pdb_id.'
+    exit (0)
+
+print(args.pdb_id)
+
+
 
 conn = sqlite3.connect('pdb_coordinates.sqlite')
 cur = conn.cursor()
 
-fn_pdb = raw_input('Enter the pdb filename: ')
-fh_pdb = open(fn_pdb)
+fn_pdbid = path.join(args.directory,args.pdb_id,'%s.pdb' % args.pdb_id)
+
+fh_pdb = open(fn_pdbid)
 for line in fh_pdb:
   line = line.split()
   if 'HEADER' in line:
