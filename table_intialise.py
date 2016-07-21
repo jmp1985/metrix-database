6aad8a82ff40e6758dc679e0766f6224d9b466b0
dj325 <dominic.jaques@gmail.com>
2016-07-21 16:37:56 +0100

    Update table_intialise.py

--- table_intialise.py
+++ table_intialise.py
@@ -1,28 +1,45 @@
-
-# Locates _scalr_statistics
 import sqlite3
 import json
+import sys
+import os.path
+
+print sys.argv
 
 conn = sqlite3.connect('pdb_coordinates.sqlite')
 cur = conn.cursor()
 
 cur.executescript('''
 DROP TABLE IF EXISTS PDB_id;
+DROP TABLE IF EXISTS PDB_id_Stats;
 DROP TABLE IF EXISTS High_Res_Stats;
 DROP TABLE IF EXISTS Low_Res_Stats;
 DROP TABLE IF EXISTS Mid_Res_Stats;
+
+
 CREATE TABLE PDB_id (
-    id  TEXT UNIQUE,
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    pdb_id  TEXT UNIQUE,
     data_type TEXT
 );
+
+CREATE TABLE PDB_id_Stats (
+    pdb_id_id INTEGER,
+    FOREIGN KEY (pdb_id_id) REFERENCES PDB_id(id)
+);
+
 CREATE TABLE High_Res_Stats (
-    pdb_id  TEXT UNIQUE
+    pdb_id_id INTEGER,
+    FOREIGN KEY (pdb_id_id) REFERENCES PDB_id(id)
 );
+
 CREATE TABLE Low_Res_Stats (
-    pdb_id  TEXT UNIQUE
+    pdb_id_id INTEGER,
+    FOREIGN KEY (pdb_id_id) REFERENCES PDB_id(id)
 );
+
 CREATE TABLE Mid_Res_Stats (
-    pdb_id  TEXT UNIQUE
+    pdb_id_id INTEGER,
+    FOREIGN KEY (pdb_id_id) REFERENCES PDB_id(id)
 )
 ''')
 
@@ -50,6 +67,7 @@ names = {
   'Total unique' : 'totalunique'
 }
 # Completeness only has a value for High_Res_Stats
+
 
 for stat in names.values():
     cur.executescript('''
