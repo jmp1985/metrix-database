bea9d2d4dd3ecf652e26217b997d8ec284d7f044
dj325 <dominic.jaques@gmail.com>
2016-08-12 11:30:46 +0100

    Update pdb_id_writer.py

--- pdb_id_writer.py
+++ pdb_id_writer.py
@@ -1,9 +1,15 @@
 # Writes all the pdb ids to a txt file so they can be recursively parsed into the pdb_parser
-
+import argparse
 from os import listdir
 from os.path import isfile, join
 
-pdb_list =  listdir('PDB_coordinates')
+parser = argparse.ArgumentParser(description='command line argument')
+parser.add_argument('--directory', dest = 'directory', type=str,
+                    help='the pdb id directory', 
+                    default = '/dls/mx-scratch/melanie/for_METRIX/JCSG_SAD_data_rerun9')
+
+args = parser.parse_args()
+pdb_list =  listdir(args.directory)
 
 pdb_id_list = []
 for pdb in pdb_list:
