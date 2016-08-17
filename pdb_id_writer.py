9b71fd74f1e6a61b93f1bec745acf669b6038c0c
dj325 <dominic.jaques@gmail.com>
2016-08-17 09:38:46 +0100

    Update pdb_id_writer.py

--- pdb_id_writer.py
+++ pdb_id_writer.py
@@ -1,22 +1,23 @@
-# Writes all the pdb ids to a txt file so they can be recursively parsed into the pdb_parser
 import argparse
 from os import listdir
 from os.path import isfile, join
 
-parser = argparse.ArgumentParser(description='command line argument')
-parser.add_argument('--directory', dest = 'directory', type=str,
-                    help='the pdb id directory', 
-                    default = '/dls/mx-scratch/melanie/for_METRIX/JCSG_SAD_data_rerun9')
+fh= open('pdb_id_list.txt', 'w')
 
+parser = argparse.ArgumentParser(description='command line argument')
+parser.add_argument('--directory', dest = 'directory', type = str, help = 'the directory containing pdbs to parse', default = '/dls/mx-scratch/melanie/for_METRIX/JCSG_SAD_data_rerun9')
 args = parser.parse_args()
+
 pdb_list =  listdir(args.directory)
 
 pdb_id_list = []
 for pdb in pdb_list:
-    if len(pdb) == 4:
-        pdb_id_list.append(pdb)
-    else: 
-        continue
-
+  if len(pdb) == 4:
+    pdb_id_list.append(pdb)
+  else:
+    pass
 for pdb in pdb_id_list:
-    print pdb
+  fh.write(pdb)
+  fh.write('\n')
+  print pdb
+fh.close()
